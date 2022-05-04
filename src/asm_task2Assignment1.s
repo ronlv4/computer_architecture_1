682f079c082300a89cdaafac275bb65d1215181d
Ron Levi <ronlv4@gmail.com>
2022-05-04 14:07:42 +0300

    all works

--- src/asm_task2Assignment1.s
+++ src/asm_task2Assignment1.s
@@ -3,6 +3,7 @@ section	.rodata			; we define (global) read-only variables in .rodata section
 
 section .bss			; we define (global) uninitialized variables in .bss section
 	an: resb 12		; enough to store integer in [-2,147,483,648 (-2^31) : 2,147,483,647 (2^31-1)]
+	temp: resd 1
 
 section .text
 	global convertor
@@ -11,12 +12,45 @@ section .text
 convertor:
 	push ebp
 	mov ebp, esp	
-	pushad			
+	pushad
 
-	mov ecx, dword [ebp+8]	; get function argument (pointer to string)
+	mov eax, dword [ebp+8]  ; get function argument (pointer to string)
+	xor ebx, ebx
+        xor ecx, ecx
+	xor edx, edx
 
-	; your code comes here...
+extractFigure:
+	mov byte bl,[eax]
+	inc eax
+        cmp ebx, 0            ; compare to null terminator
+        je exit
+        cmp ebx, 0xA          ; compare to newline
+        je exit
+        sub ebx, '0'
+	
+	xor ecx, ecx
+        mov ecx, 4
+printFigure:
+	mov dword [temp], ebx
+        and dword [temp], ecx
+        jnz add1
 
+add0:
+        mov byte [an+edx], '0'
+        inc edx
+	shr ecx, 1
+        jnz printFigure
+        jmp extractFigure
+
+add1:
+        mov byte [an+edx], '1'
+        inc edx
+	shr ecx, 1
+        jnz printFigure        
+        jmp extractFigure
+
+exit:
+	mov byte [an+edx], 0x0
 	push an			; call printf with 2 arguments -  
 	push format_string	; pointer to str and pointer to format string
 	call printf
@@ -26,3 +60,4 @@ convertor:
 	mov esp, ebp	
 	pop ebp
 	ret
+
--- src/main_task2Assignment1.c
+++ src/main_task2Assignment1.c
@@ -3,12 +3,33 @@
 					/* enough to get 32-bit string + '\n' + null terminator */
 extern int convertor(char* buf);
 
+void string(char* buf)
+{
+	printf("%s\n", buf);
+	
+}
+
+void number(int num)
+{
+	printf("got the number %d\n", num);
+}
+
+void ch(char c)
+{
+	printf("got the character %c\n", c);
+}
+
 int main(int argc, char** argv)
 {
-  char buf[MAX_LEN ];
- 
+  char buf[MAX_LEN ]; 
+
   fgets(buf, MAX_LEN, stdin);		/* get user input string */ 
-  convertor(buf);			/* call your assembly function */
+  while (buf[0] != 'q')
+  {
+	  convertor(buf);			/* call your assembly function */
+	  fgets(buf, MAX_LEN, stdin);		/* get user input string */ 
+  }
+
 
   return 0;
 }
